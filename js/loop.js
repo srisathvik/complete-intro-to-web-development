8a0bf52ecaef27e90a1ade77ba2b1ab6bf825bf7
srisathvik <komminenisathvik@gmail.com>
2023-08-10 12:56:07 +0530

    js and changes

---
+++ js/loop.js
@@ -0,0 +1,15 @@
+let timesToRepeat = 10;
+
+let answer = '';
+let h = 'h';
+
+for(let i = 0; i < timesToRepeat; i++){
+    answer = answer + "🐶";
+}
+if (true){
+    let number = 5;
+    h = 'something else';
+}
+console.log(answer);
+// console.log(number);
+console.log(h);
---
+++ scrap/js/jsExperiment.js
@@ -0,0 +1,3 @@
+const monthlyRent = 600;
+const yearlyRent = monthlyRent * 12;
+console.log(yearlyRent);
